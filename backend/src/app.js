b908a77353c5cec1b6f24e7211440778fd537dbd
Eric Carey <eric.w.carey@gmail.com>
2025-02-24 15:47:22 -0500

    Updated build

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,6 +12,7 @@ const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
 const bookRouter_1 = __importDefault(require("./routes/bookRouter"));
 const suggestedBookRouter_1 = __importDefault(require("./routes/suggestedBookRouter"));
 const config_1 = require("./utils/config");
+const FRONTEND_URL = process.env.RAILWAY_PRIVATE_DOMAIN;
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({
     extended: true,
@@ -21,14 +22,16 @@ const limiter = (0, express_rate_limit_1.default)({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // Limit each IP to 100 requests per windowMs
 });
+const allowedOrigins = [FRONTEND_URL].filter((origin) => Boolean(origin));
+;
 const corsOptions = {
-    origin: "http://localhost:3000", //TODO: update to frontend domain (for production)
+    origin: allowedOrigins,
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
 };
 app.use(limiter);
 app.use((0, helmet_1.default)()); //helmet for security middleware
-app.use((0, cors_1.default)()); //TODO: switch to (corsOptions)
+app.use((0, cors_1.default)(corsOptions)); //TODO: switch to (corsOptions)
 app.get("/", (req, res) => {
     res.json({ message: "success" });
 });
@@ -46,6 +49,6 @@ app.use((err, req, res, next) => {
     return;
 });
 // Start HTTPS server
-http.createServer(app).listen(config_1.PORT, () => {
-    console.log(`Secure server is running at http://localhost:${config_1.PORT}`);
+http.createServer(app).listen(config_1.PORT, '0.0.0.0', () => {
+    console.log(`Secure server is running at http://0.0.0.0:${config_1.PORT}`);
 });
