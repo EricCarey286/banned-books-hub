a137995d0db4976ebc11f166a5a4694aa2a25743
Eric Carey <eric.w.carey@gmail.com>
2025-02-27 12:08:54 -0500

    Updated Frontend URL

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ import { AppError } from "./utils/helper";
 
 import { Request, Response, NextFunction } from 'express';
 
-const FRONTEND_URL = process.env.RAILWAY_PRIVATE_DOMAIN;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 console.log('Frontend URL = ' + FRONTEND_URL);
 
 app.use(express.json());
