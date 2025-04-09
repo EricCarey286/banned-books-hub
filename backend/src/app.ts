dde60e36f0f8196908bc0ac5300871a1b3aeff76
Eric Carey <eric.w.carey@gmail.com>
2025-04-09 15:25:34 -0400

    Fixed rate-limit issue

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -57,6 +57,7 @@ const authenticate = (req: AuthRequest, res: Response, next: NextFunction): void
 };
 
 //express-rate-limit for overload prevention
+app.set('trust proxy', 1); // or true
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per windowMs
